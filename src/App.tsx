a7774909c8e3239db364932d204cddb3f440071c
murad-dabbous <moradyousef04@gmail.com>
2026-02-08 08:52:05 +0000

    React & TypeScript initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const num: number = 3;
+console.log("Hello from ts,", num);
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
